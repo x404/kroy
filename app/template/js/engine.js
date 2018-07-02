7688c63456f23aa1120c5b155eb508d7f574c426
x404 <kronos2003@gmail.com>
2018-07-02 20:46:40 +0300

    Каталог и карточка товара

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -61,4 +61,68 @@ $(document).ready(function(){
 			media : {}
 		}
 	});
+
+
+
+	/*  =product plus/minus */
+	/* orders */
+	$("#pcount").keydown(function(event) {
+		// Разрешаем: backspace, delete, tab и escape
+		if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+			 // Разрешаем: Ctrl+A
+			(event.keyCode == 65 && event.ctrlKey === true) ||
+			 // Разрешаем: home, end, влево, вправо
+			(event.keyCode >= 35 && event.keyCode <= 39)) {
+				 // Ничего не делаем
+			 	$("#pcount").val("1");
+				 return;
+		}
+		else {
+			// Убеждаемся, что это цифра, и останавливаем событие keypress
+			if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+				event.preventDefault();
+			}  
+		}
+	});
+
+	var quantity = $("#quantity");
+	$('.count #plus').click(function(e){
+		e.preventDefault();
+		quantity.val(parseInt(quantity.val())+1);
+		$('.disabled').removeClass('disabled');
+		// подсчет общей суммы
+	})
+
+	$('#quantity').val('1');
+	$('.count #minus').click(function(e){
+		e.preventDefault();
+		var count = parseInt(quantity.val())-1;
+		if (count <= 1) {
+			$(this).addClass('disabled');
+			count = 1;
+		};		
+		quantity.val(count);
+	})
+	/*  =/product plus/minus */
+
+
 });
+
+
+
+// card thumbs
+var thumbsurl = document.querySelectorAll('.card__thumbs a');
+for (index = 0; index < thumbsurl.length; index++) {
+    thumb = thumbsurl[index];
+    thumb.addEventListener('click', clickHandler);
+}
+
+function clickHandler(event) {
+    let $newSrc = this.href;
+    document.getElementById('mainImg').src = $newSrc;
+    document.querySelector('.card__thumbs a.active').classList.remove('active');
+    this.classList.add('active');
+    event.preventDefault();
+}
+// =/card thumbs
+
